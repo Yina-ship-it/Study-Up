d60ca1d23779ab900aa5e8e753d78edbc311c033
Yina-ship-it <najdenov7731@gmail.com>
2023-03-25 20:39:31 +0500

    Initial commit

---
+++ src/test/java/com/team4822/studyup/StudyUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4822.studyup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
